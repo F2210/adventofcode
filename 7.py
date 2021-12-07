875b830c18e3cfe2a2f899603a7568d57107c5ad
Jacco <jaccobroeren@freedom.nl>
2021-12-07 07:03:29 +0100

    Day 7 - easy peasy

--- 7.py
+++ 7.py
@@ -1,3 +1,27 @@
+import matplotlib.pyplot as plt
+
 with open('input/7.txt', 'r') as file:
 
-    lines = file.readlines()
+    initialstate = [int(i) for i in file.readlines()[0].strip().split(",")]
+
+    initialstate = sorted(initialstate)
+
+    fuelcostlista = []
+    fuelcostlistb = []
+
+    fuelcosta = []
+    fuelcostb = []
+
+    for i in range(2000):
+        for state in initialstate:
+            x = abs(i - state)
+            cost = (x*(x+1))/2
+            fuelcostb.append(cost)
+            fuelcosta.append(x)
+        fuelcostlistb.append(sum(fuelcostb))
+        fuelcostlista.append(sum(fuelcosta))
+        fuelcosta = []
+        fuelcostb = []
+
+    print(int(min(fuelcostlista)))
+    print(int(min(fuelcostlistb)))
